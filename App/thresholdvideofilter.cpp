ca31dd77e4ac057056103035128acf6c2ebba40d
medyakovvit <medyakovvit@gmail.com>
2016-01-19 23:57:30 +0400

    Implement ThresholdVideoFilter

--- App/thresholdvideofilter.cpp
+++ App/thresholdvideofilter.cpp
@@ -1,23 +1,60 @@
 #include "thresholdvideofilter.h"
 
+#include "opencv2/imgproc.hpp"
+
 ThresholdVideoFilter::ThresholdVideoFilter() :
-    m_thresold(0.0),
+    m_threshold(0.0),
     m_maxValue(255.0)
 {
 
 }
 
-void ThresholdVideoFilter::filter(Data *)
+void ThresholdVideoFilter::setThreshold(double threshold)
 {
+    if(m_threshold == threshold)
+        return;
+
+    m_threshold = threshold;
+}
+
+void ThresholdVideoFilter::filter(Data *data)
+{
+    cv::Mat thresholded;
+
+    if (!data)
+    {
+        m_data.setMat(thresholded);
+        return;
+    }
+
+    MatData* matData = static_cast<MatData*>(data);
+    cv::Mat image = matData->getMat();
+
+    if(image.empty())
+    {
+        m_data.setMat(thresholded);
+        return;
+    }
+
+    if(image.type() != CV_8UC1)
+    {
+        m_data.setMat(thresholded);
+        return;
+    }
+
+    cv::threshold(image, thresholded, this->m_threshold, 255.0, cv::THRESH_BINARY);
+
+    m_data.setMat(thresholded);
+
     return;
 }
 
 QList<Data::DataType> ThresholdVideoFilter::supportedInDataTypes()
 {
-    return QList<Data::DataType>() << Data::CV_MAT;
+    return QList<Data::DataType>() << Data::CV_MAT_8UC1;
 }
 
 QList<Data::DataType> ThresholdVideoFilter::supportedOutDataTypes()
 {
-    return QList<Data::DataType>() << Data::CV_MAT;
+    return QList<Data::DataType>() << Data::CV_MAT_8UC1;
 }
--- App/thresholdvideofilter.h
+++ App/thresholdvideofilter.h
@@ -2,21 +2,27 @@
 #define THRESHOLDVIDEOFILTER_H
 
 #include "videofilter.h"
+#include "matdata.h"
 
 class ThresholdVideoFilter : public VideoFilter
 {
 public:
     ThresholdVideoFilter();
 
+    double threshold(){return m_threshold;}
+    void setThreshold(double threshold);
+
     // VideoFilter interface
 public:
     void filter(Data *);
+    Data* getData(){return &m_data;}
     QList<Data::DataType> supportedInDataTypes();
     QList<Data::DataType> supportedOutDataTypes();
 
 protected:
-    double m_thresold;
+    double m_threshold;
     double m_maxValue;
+    MatData m_data;
 };
 
 #endif // THRESHOLDVIDEOFILTER_H
